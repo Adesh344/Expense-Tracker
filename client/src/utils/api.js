8ac7b3257881a6eddeb850ab1031a8dca757f3f1
Adesh344 <agourav311@gmail.com>
2025-12-24 05:29:01 +0530

    Files for frontend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,12 +1,27 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
+// Add token to requests if it exists
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Handle response errors
 api.interceptors.response.use(
   (response) => response,
   (error) => {
